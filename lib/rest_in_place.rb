8f87743a7989d91a1e7fecd5dc8438e654284b52
Ben Woosley <ben.woosley@gmail.com>
2012-03-30 19:34:04 -0700

    Properly match Mongoid ids when generated by dom_id

--- lib/rest_in_place.rb
+++ lib/rest_in_place.rb
@@ -5,13 +5,25 @@ module RestInPlace
     # no ruby code here!
   end
   
-  def self.include_root_in_json?
-    if defined?(Mongoid)
-      Mongoid.config.include_root_in_json
-    elsif defined?(ActiveRecord)
-      ActiveRecord::Base.include_root_in_json
-    else
-      false
+  class << self
+    def include_root_in_json?
+      if defined?(Mongoid)
+        Mongoid.config.include_root_in_json
+      elsif defined?(ActiveRecord)
+        ActiveRecord::Base.include_root_in_json
+      else
+        false
+      end
+    end
+
+    def id_chars
+      if defined?(Mongoid)
+        '[a-f0-9]'
+      elsif defined?(ActiveRecord)
+        '\d'
+      else
+        raise NotImplementedError, 'Neither ActiveRecord nor Mongoid was detected'
+      end
     end
   end
 end
